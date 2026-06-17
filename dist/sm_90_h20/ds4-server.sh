2a925d376eb5e96ac57333524015c7b668d91b0d
rootuc <rootuc@notunixchan>
2026-06-17 18:11:21 +0800

    Updated at Wed Jun 17 06:11:20 PM CST 2026

--- dist/sm_90_h20/ds4-server.sh
+++ dist/sm_90_h20/ds4-server.sh
@@ -4,22 +4,32 @@ export DS4_CUDA_COPY_MODEL=1
 
 
 normal(){
+export DS4_CUDA_Q8_F16_CACHE_MB=1024
 #export DS4_CUDA_Q8_F16_CACHE_MB=2048
-export DS4_CUDA_Q8_F16_CACHE_MB=3072
+#export DS4_CUDA_Q8_F16_CACHE_MB=3072
 #export DS4_CUDA_Q8_F16_CACHE_MB=4096 # OOM on H20 96GB
 }
-#normal
+normal
 
 mtp(){
 OPTS=" --mtp DeepSeek-V4-Flash-MTP-Q4K-Q8_0-F32.gguf $OPTS "
 }
-mtp
+#mtp
 
 EXE="ds4-server"
 
 #OPTS=" --threads 4 $OPTS "
 OPTS=" --threads 30 $OPTS "
-OPTS=" --port 8888 --prefill-chunk 8192 $OPTS "
+
+CTX=16384
+#CTX=8192
+#OPTS=" --ctx $CTX $OPTS "
+
+PREFILL=8192
+PREFILL=4096
+OPTS=" --prefill-chunk $PREFILL $OPTS "
+
+OPTS=" --port 8888 $OPTS "
 
 LOG="$EXE-$(date +%s).log"
 
