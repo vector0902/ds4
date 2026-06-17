33615eb2b290b9763e17ae5ec765964f17978105
rootuc <rootuc@notunixchan>
2026-06-17 13:40:41 +0800

    Updated at Wed Jun 17 01:40:41 PM CST 2026

--- dist/sm_90_h20/ds4-server.sh
+++ dist/sm_90_h20/ds4-server.sh
@@ -2,9 +2,18 @@
 # put model into VRAM
 export DS4_CUDA_COPY_MODEL=1
 
+
+normal(){
 #export DS4_CUDA_Q8_F16_CACHE_MB=2048
 export DS4_CUDA_Q8_F16_CACHE_MB=3072
 #export DS4_CUDA_Q8_F16_CACHE_MB=4096 # OOM on H20 96GB
+}
+#normal
+
+mtp(){
+OPTS=" --mtp DeepSeek-V4-Flash-MTP-Q4K-Q8_0-F32.gguf $OPTS "
+}
+mtp
 
 EXE="ds4-server"
 
