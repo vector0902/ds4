8bbccba92c0c9de93eff1b9ca679da12e2deef68
rootuc <rootuc@notunixchan>
2026-06-17 13:14:43 +0800

    Updated at Wed Jun 17 01:14:43 PM CST 2026

--- dist/sm_90_h20/ds4-server.sh
+++ dist/sm_90_h20/ds4-server.sh
@@ -1,7 +1,14 @@
 
+# put model into VRAM
+export DS4_CUDA_COPY_MODEL=1
+
+#export DS4_CUDA_Q8_F16_CACHE_MB=2048
+export DS4_CUDA_Q8_F16_CACHE_MB=3072
+#export DS4_CUDA_Q8_F16_CACHE_MB=4096 # OOM on H20 96GB
+
 EXE="ds4-server"
 
-OPTS=" --port 8888 $OPTS "
+OPTS=" --port 8888 --prefill-chunk 8192 --threads 4 $OPTS "
 
 LOG="$EXE-$(date +%s).log"
 
